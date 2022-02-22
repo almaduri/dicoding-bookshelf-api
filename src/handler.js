372595b30de9267fe698cd69492d657aac21c210
almaduri <ali.mdrw@gmail.com>
2022-02-22 20:30:54 +0800

    edit routes.js

---
+++ src/handler.js
@@ -0,0 +1,30 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+
+};
+
+const getAllBooksHandler = () => {
+
+};
+
+const getBookByIdHandler = (request, h) => {
+
+};
+
+const editBookByIdHandler = (request, h) => {
+
+};
+
+const deleteBookByIdHandler = (request, h) => {
+
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+};
